cae43137652b4b20f76e09d99bcd883728f560d7
Micah Zaayer <micahzaayer@gmail.com>
2019-10-11 09:15:20 -0700

    add show file to vendor class as well as find method.

--- Bakery/Controllers/VendorsController.cs
+++ Bakery/Controllers/VendorsController.cs
@@ -8,8 +8,30 @@ namespace Bakery.Controllers
     public class VendorsController : Controller
     {
 
-
-
-
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+        return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+        Vendor newVendor = new Vendor(vendorName, vendorDescription);
+        return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+        List<Vendor> allVendors = Vendor.GetAll();
+        return View(allVendors);
+    }
+    
+    [HttpPost("/vendors/{id}")]
+   
+    public ActionResult Show(int id)
+    {
+        Vendor foundVendor = Vendor.Find(id);
+        return View(foundVendor);
+    }
     }
 }
--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -5,23 +5,15 @@ namespace Bakery.Models
     public class Order
     {
         public string Description { get; set; }
-        private static List<Item> _instances = new List<Item> { };
+
 
         public Order(string description)
         {
             Description = description;
-            _instances.Add(this);
-        }
-
-        public static List<Item> GetAll()
-        {
-            return _instances;
+            //Add to Vendor1.orders
+            // Order.Add(this);
         }
 
 
-        public static Item Find(int searchId)
-        {
-            return _instances[searchId - 1];
-        }
     }
 }
