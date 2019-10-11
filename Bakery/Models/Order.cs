7a47dd6e079fde02c437fe1ad1b349a64f9aa0a9
Micah Zaayer <micahzaayer@gmail.com>
2019-10-11 12:08:33 -0700

    add more order details to cshtml pages.

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -5,15 +5,22 @@ namespace Bakery.Models
 {
     public class Order
     {
+        public string OrderTitle { get; set; }
+        public string OrderDate { get; set; }
         public string OrderDescription { get; set; }
+        public int OrderPrice { get; set; }
+
         public int Id { get; }
         // public List<Order> Orders { get; set; }
         public static List<Order> OrderList = new List<Order> { };
 
 
-        public Order(string orderDescription)
+        public Order(string orderTitle, string orderDate, string orderDescription)
         {
+            OrderTitle = orderTitle;
+            OrderDate = orderDate;
             OrderDescription = orderDescription;
+            OrderPrice = 0;
             OrderList.Add(this);
             Id = OrderList.Count;
             // OrderList = new List<Order>{};
