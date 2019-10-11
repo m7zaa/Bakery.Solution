18d2407b370b189cfdfeeb64acc57fbabcadd199
Micah Zaayer <micahzaayer@gmail.com>
2019-10-11 11:50:39 -0700

    fix order detail links.

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -7,18 +7,17 @@ namespace Bakery.Models
     {
         public string OrderDescription { get; set; }
         public int Id { get; }
-        public List<Order> Orders { get; set; }
+        // public List<Order> Orders { get; set; }
         public static List<Order> OrderList = new List<Order> { };
 
 
         public Order(string orderDescription)
         {
             OrderDescription = orderDescription;
+            OrderList.Add(this);
             Id = OrderList.Count;
-            OrderList = new List<Order>{};
+            // OrderList = new List<Order>{};
 
-            //Add to Vendor1.orders
-            // Order.Add(this);
         }
         public static Order Find(int searchId)
         {
