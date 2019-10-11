7bdd097d033ac251bbc38ca905ccb593323439d6
Micah Zaayer <micahzaayer@gmail.com>
2019-10-11 16:53:36 -0700

    add non-functional cost methods.

--- Bakery/Controllers/VendorsController.cs
+++ Bakery/Controllers/VendorsController.cs
@@ -35,11 +35,22 @@ namespace Bakery.Controllers
             return View(model);
         }
         [HttpPost("/vendors/{vendorId}/orders")]
-        public ActionResult Create(int vendorId, string orderTitle, string orderDate, string orderDescription, int orderPrice)
+        public ActionResult Create(int vendorId, string orderTitle, string orderDate, string orderDescription, string cookieQuant, string breadQuant)
         {
+
+            Order newOrder = new Order(orderTitle, orderDate, orderDescription, cookieQuant, breadQuant);
+
+            int breadTotal = 0;
+            int cookieTotal = 0;
+            int cookieQuants = int.Parse(cookieQuant);
+            int breadQuants = int.Parse(breadQuant);
+
+            Order.BreadCost(breadQuants, breadTotal);
+            Order.CookieCost(cookieQuants, cookieTotal);
+            newOrder.OrderPrice = (cookieTotal + breadTotal);
+
             Dictionary<string, object> model = new Dictionary<string, object>();
             Vendor foundVendor = Vendor.Find(vendorId);
-            Order newOrder = new Order(orderTitle, orderDate, orderDescription);
             foundVendor.AddOrder(newOrder);
             List<Order> vendorOrders = foundVendor.Orders;
             model.Add("orders", vendorOrders);
--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Bakery.Models;
+using System;
 
 namespace Bakery.Models
 {
@@ -9,15 +10,24 @@ namespace Bakery.Models
         public string OrderDate { get; set; }
         public string OrderDescription { get; set; }
         public int OrderPrice { get; set; }
+        public string CookieQuant { get; set; }
+        public int CookieTotal { get; set; }
+        public string BreadQuant { get; set; }
+        public int BreadTotal { get; set; }
+
         public int Id { get; }
         public static List<Order> OrderList = new List<Order> { };
 
-        public Order(string orderTitle, string orderDate, string orderDescription)
+        public Order(string orderTitle, string orderDate, string orderDescription, string cookieQuant, string breadQuant)
         {
             OrderTitle = orderTitle;
             OrderDate = orderDate;
             OrderDescription = orderDescription;
             OrderPrice = 0;
+            CookieQuant = cookieQuant;
+            CookieTotal = 0;
+            BreadQuant = breadQuant;
+            BreadTotal = 0;
             OrderList.Add(this);
             Id = OrderList.Count;
         }
@@ -25,6 +35,17 @@ namespace Bakery.Models
         public static Order Find(int searchId)
         {
             return OrderList[searchId - 1];
+        }
+        public static int BreadCost(int breadQuant, double breadTotal)
+        {
+            breadTotal = Math.Ceiling((double)breadQuant / 1.5) * 5;
+            return (int)breadTotal;
+        }
+
+        public static int CookieCost(int cookieQuant, double cookieTotal)
+        {
+            cookieTotal = Math.Ceiling(((double)cookieQuant / 3) * 5);
+            return (int)cookieTotal;
         }
     }
 }
